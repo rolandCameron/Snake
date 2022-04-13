0b618ee15b23d8f4325ea5abd65ce647d88e8065
rolandCameron <99928788+rolandCameron@users.noreply.github.com>
2022-04-13 14:29:56 +1200

    Decomposition and requirements
    If the requirements are met, it should be relatively simple to connect the program to a NN.

---
+++ Main.py
@@ -0,0 +1,13 @@
+"""
+Program Decomposition and requirements.
+
+Must be able to return the number of turns the user usrvived.
+Must have a flexible input system (Not just button presses)
+Only show the screen if a certain variable is true
+
+1. Set up an empty grid, with the snake starting in the correct position
+2. Allow inputs to change direction
+3. Randomly place a fruit each time one is eaten, and make the snake a tile longer.
+
+4. Give board information each frame (so that a NN can be trained on it in the future.)
+"""
